9750bb1a1eee77497f2cfd5cf00f28d8244e53f5
rahul singh <rahul.cyber@outlook.com>
2023-10-11 19:36:13 +0530

    s

--- src/components/Client/Contact.jsx
+++ src/components/Client/Contact.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ContactImage from "../../assets/images/contact.svg";
 import PaperClip from "../../assets/images/icons/svg/PaperClip";
+import Location from "../../icons/Location";
+import ContactIcon from "../../assets/images/icons/svg/ContactIcon";
+import MailIcon from "../../assets/images/icons/svg/MailIcon";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
@@ -83,8 +87,27 @@ const Contact = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col-md-4">
-          <div className="shadow bg-white mt-5 br-20 p-4"></div>
+        <div className="col-md-6">
+          <div className="shadow bg-white mt-5 br-20 p-4 address">
+            <h5 className="mb-4">Contact Info</h5>
+            <div class="d-flex gap-3 align-items-center mb-3">
+              <Location height="2rem" fill="#78909c" />
+              <p class="fw-bold fs-6 ">
+                D -113, Basement Floor, Sector -63, Noida , (U.P) <br />
+                Pin-Code - 201301, India
+              </p>
+            </div>
+            <div class="d-flex gap-3 align-items-center  mb-3">
+              <ContactIcon height="1.5rem" fill="#78909c" />
+              <Link to="tel:919716164902">
+                <p class="fw-bold fs-6 ">+919991112222</p>
+              </Link>
+            </div>
+            <div class="d-flex gap-3 align-items-center  mb-3">
+              <MailIcon height="1.5rem" fill="#78909c" />
+              <p class="fw-bold fs-6 ">info@medi.com</p>
+            </div>
+          </div>
         </div>
         <div className="col-md-4"></div>
         <div className="col-md-4"></div>
